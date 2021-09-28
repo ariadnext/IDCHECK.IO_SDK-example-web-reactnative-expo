288706029376568e547cfd0d8fdaf1ef3acdbbc4
Romain HERRY <romain.herry@ariadnext.com>
2021-09-28 18:06:50 +0200

    chore: update yarn.lock

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { WebView } from "react-native-webview";
-import { Camera } from "expo-camera";
+import React, { useState, useEffect } from 'react';
+import { WebView } from 'react-native-webview';
+import { Camera } from 'expo-camera';
 
 const WEBSDK_LINK =
-  "https://sdkweb-test.idcheck.io/v2/ariadnext/829d3898-9597-4e74-a02e-21002d5212ab";
+  'https://sdkweb-test.idcheck.io/v2/ariadnext/942e56a5-d49b-4fe6-8918-24477726c109';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === "granted");
+      setHasPermission(status === 'granted');
     })();
   }, []);
 
