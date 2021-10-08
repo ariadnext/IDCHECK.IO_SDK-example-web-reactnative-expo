64b9b6cc6da5f2e3d17903a576c9eb3798938ad9
Romain HERRY <romain.herry@ariadnext.com>
2021-10-08 17:23:18 +0200

    fix: ask audio permissions

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { WebView } from 'react-native-webview';
 import { Camera } from 'expo-camera';
+import { Audio } from 'expo-av';
 
 const WEBSDK_LINK =
   'https://sdkweb-test.idcheck.io/v2/ariadnext/942e56a5-d49b-4fe6-8918-24477726c109';
@@ -10,7 +11,9 @@ export default function App() {
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
+      const { status: audioStatus } = await Audio.requestPermissionsAsync();
       setHasPermission(status === 'granted');
+      setHasPermission(audioStatus === 'granted');
     })();
   }, []);
 
